c1f8b7ccff74e1cdfbf0b27f058fee1b0b91d392
hasmikmikael <hasmik.mikaelyan.goodcredit@gmail.com>
2026-08-18 23:53:01 +0400

    Creating a gRPC client

---
+++ GrpcClientApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using GrpcClientApp;
+
+// list of words for translation 
+var words = new List<string>() { "red", "yellow", "green" };
+
+// creating a channel for exchanging messages with the server 
+// parameter — gRPC server address 
+using var channel = GrpcChannel.ForAddress("https://localhost:7178");
+
+// creating a client 
+var client = new Translator.TranslatorClient(channel);
+
+// sending each word to the service to obtain a translation 
+foreach (var word in words)
+{
+    // preparing the message for sending 
+    Request request = new Request { Word = word };
+
+    // sending a message and receive a response 
+    Response response = await client.TranslateAsync(request);
+
+    // outputing the word and its translation 
+    Console.WriteLine($"{response.Word} : {response.Translation}");
+}

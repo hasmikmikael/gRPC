a31a87153c3f7898760e3bfc1a71b46b7fcccf9d
hasmikmikael <hasmik.mikaelyan.goodcredit@gmail.com>
2026-08-18 19:05:59 +0400

    Console client for gRPC service

---
+++ GreeterClientApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+using GreeterClientApp;
+
+// creating a channel for exchanging messages with the server 
+// parameter — gRPC server address 
+using var channel = GrpcChannel.ForAddress("https://localhost:7069");
+
+// creating a client 
+var client = new Greeter.GreeterClient(channel);
+Console.Write("Enter name: ");
+var name = Console.ReadLine();
+
+// exchanging messages with the server 
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"Server response: {reply.Message}");
+Console.ReadKey();

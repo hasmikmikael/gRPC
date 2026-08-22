21d716b452bb69db6293eb75161c72266fa82dc1
hasmikmikael <hasmik.mikaelyan.goodcredit@gmail.com>
2026-08-22 23:20:36 +0400

    Building a CRUD interface - Client project - Obtaining a list

---
+++ CRUDClientApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Crud;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+// creating a channel for exchanging messages with the server. 
+// parameter — gRPC server address 
+using var channel = GrpcChannel.ForAddress("https://localhost:7017");
+
+// creating a client 
+var client = new UserService.UserServiceClient(channel);
+
+// obtaining a list 
+ListReply users = await client.ListUsersAsync(new Google.Protobuf.WellKnownTypes.Empty());
+
+foreach (var user in users.Users)
+{
+    Console.WriteLine($"{user.Id}. {user.Name} - {user.Age}");
+}
+Console.WriteLine("");
